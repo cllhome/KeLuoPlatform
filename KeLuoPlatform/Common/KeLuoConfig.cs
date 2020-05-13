644c65e1be54cce54ca1a6eccad2f70128cc98bb
DESKTOP-ASTINI9\cuilin <cllhome@163.com>
2020-05-13 16:36:04 +0800

    目录调整

---
+++ KeLuoPlatform/Common/KeLuoConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace KeLuoPlatform.Common
+{
+    public class KeLuoConfig
+    {
+        private static IConfigurationRoot _config;
+
+        public static IConfigurationRoot Load()
+        {
+            if (_config == null)
+            {
+                _config = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json")
+             .Build();
+            }
+            return _config;
+        }
+    }
+}
---
+++ KeLuoPlatform/Common/KeLuoResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeLuoPlatform.Common
+{
+    public class KeLuoResult
+    {
+        public bool Status { get; set; }
+        public Object Result { get; set; }
+        public string Msg { get; set; }
+    }
+}
